a06ec82a64925e1858734178dda815a68c15d0d9
sachinlodhi <sachinlodhi8614@gmail.com>
2022-08-21 20:10:49 +0530

    rater agreement initial commit

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-#this is the test
+from sklearn.metrics import cohen_kappa_score
+import random
+
+# number of criteria
+criterion = 7
+threshold = 0.7
+
+for i in range(1000):
+    # number of raters [currently only 2 raters]
+    rater1 = [random.randint(0, 1) for j in range(criterion)]
+    rater2 = [random.randint(0, 1) for j in range(criterion)]
+    ans = cohen_kappa_score(rater1, rater2)
+    if ans>threshold:
+        print(rater1)
+        print(rater2)
+        print(ans)
+
+
